d375002734ec1625e34b9e089d5181021a730423
Dan Harper <intouch@danharper.me>
2014-04-27 19:57:19 +0100

    test title & form exists

--- test/SystemTest.php
+++ test/SystemTest.php
@@ -4,13 +4,15 @@ use Goutte\Client;
 
 class SystemTest extends \PHPUnit_Framework_TestCase {
 
+	const ROOT_URL = 'http://localhost:8080/';
+
 	/** @var \Symfony\Component\DomCrawler\Crawler */
 	private $crawler;
 
 	public function setUp()
 	{
 		$client = new Client;
-		$this->crawler = $client->request('GET', 'http://localhost:8080');
+		$this->crawler = $client->request('GET', self::ROOT_URL);
 	}
 
 	public function testItWorks()
@@ -23,11 +25,11 @@ class SystemTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals('Shorty', $this->crawler->filter('title')->text());
 	}
 
-	public function testFormPostsToSameUrl()
+	public function testFormWillPostToSelf()
 	{
 		$form = $this->crawler->filter('form')->form();
 		$this->assertEquals('POST', $form->getMethod());
-		$this->assertEquals('http://localhost:8080/', $form->getUri());
+		$this->assertEquals(self::ROOT_URL, $form->getUri());
 	}
 
 } 
