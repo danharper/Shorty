7a2d282f3bf9ef728681fc3df051b3e828bd9150
Dan Harper <intouch@danharper.me>
2014-04-28 07:43:56 +0100

    before, get !== GET !== Get; use isMethod instead

--- public/index.php
+++ public/index.php
@@ -14,17 +14,17 @@ $tagGenerator = new \Shorty\TagGenerator($urlRepository);
 
 $controller = null;
 
-if ($request->getMethod() == 'POST' && ($request->getPathInfo() == '/' || $request->getPathInfo() == ''))
+if ($request->isMethod('POST') && ($request->getPathInfo() == '/' || $request->getPathInfo() == ''))
 {
 	$controller = new \Shorty\Controller\CreateTagController($urlRepository, $tagGenerator);
 }
 
-if ($request->getMethod() == 'GET' && $request->getPathInfo() != '/' && $request->getPathInfo() != '')
+if ($request->isMethod('GET') && $request->getPathInfo() != '/' && $request->getPathInfo() != '')
 {
 	$controller = new \Shorty\Controller\TagRedirectController($urlRepository);
 }
 
-if ($request->getMethod() == 'GET' && ($request->getPathInfo() == '/' || $request->getPathInfo() == ''))
+if ($request->isMethod('GET') && ($request->getPathInfo() == '/' || $request->getPathInfo() == ''))
 {
 	$controller = new \Shorty\Controller\HomeController();
 }
