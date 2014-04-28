7ae30cf02f94aff818a0093d4f16f6b4f11fb5e3
Dan Harper <intouch@danharper.me>
2014-04-28 19:56:56 +0100

    symfony router

--- public/index.php
+++ public/index.php
@@ -1,39 +1,47 @@
 <?php
 
+use Illuminate\Container\Container;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
 require '../vendor/autoload.php';
 
 define('TEMPLATE_ROOT', '../web');
 
-$session = new \Symfony\Component\HttpFoundation\Session\Session;
+$session = new Session;
 $session->start();
 
-$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+$request = Request::createFromGlobals();
 
-$container = new \Illuminate\Container\Container();
+$container = new Container();
 $container->bind('Shorty\PdoFactory', 'Shorty\MySqlPdoFactory');
 
-$routes = [
-	['GET', '/', 'Shorty\Controller\HomeController'],
-	['POST', '/', 'Shorty\Controller\CreateTagController'],
-	['GET', '*', 'Shorty\Controller\TagRedirectController'],
-];
+$routes = new RouteCollection();
+$routes->add('create_tag', (new Route('/', ['controller' => 'Shorty\Controller\CreateTagController']))->setMethods('POST'));
+$routes->add('home', (new Route('/', ['controller' => 'Shorty\Controller\HomeController']))->setMethods('GET'));
+$routes->add('redirect_tag', (new Route('/{tag}', ['controller' => 'Shorty\Controller\TagRedirectController']))->setMethods('GET'));
 
-function findController($routes, $request) {
-	foreach ($routes as list($method, $route, $callable)) {
-		if ($request->isMethod($method) && ($request->getPathInfo() == $route || $route == '*')) {
-			return $callable;
-		}
-	}
-}
+$context = new RequestContext();
+$context->fromRequest($request);
 
-if ($callable = findController($routes, $request))
-{
-	$controller = $container->make($callable);
-	$response = $controller($request, $session);
-	$response->prepare($request)->send();
-	die;
+$matcher = new UrlMatcher($routes, $context);
+
+try {
+	$attributes = $matcher->match($request->getPathInfo());
 }
-else
-{
+catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $e) {
 	die('Unknown Request');
 }
+catch (\Symfony\Component\Routing\Exception\MethodNotAllowedException $e) {
+	die('Unknown Request');
+}
+
+$controller = $attributes['controller'];
+$controller = $container->make($controller);
+
+$response = $controller($request, $session);
+$response->prepare($request)->send();
