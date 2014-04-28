90d1e6d3de7fcff5145d2a58142b7ad191522b54
Dan Harper <intouch@danharper.me>
2014-04-28 20:02:38 +0100

    EXAMPLE - cant use ControllerResolver, already using IoC..

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use Illuminate\Container\Container;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Route;
@@ -21,17 +22,23 @@ $container = new Container();
 $container->bind('Shorty\PdoFactory', 'Shorty\MySqlPdoFactory');
 
 $routes = new RouteCollection();
-$routes->add('create_tag', (new Route('/', ['controller' => 'Shorty\Controller\CreateTagController']))->setMethods('POST'));
-$routes->add('home', (new Route('/', ['controller' => 'Shorty\Controller\HomeController']))->setMethods('GET'));
-$routes->add('redirect_tag', (new Route('/{tag}', ['controller' => 'Shorty\Controller\TagRedirectController']))->setMethods('GET'));
+$routes->add('create_tag', (new Route('/', ['_controller' => 'Shorty\Controller\CreateTagController']))->setMethods('POST'));
+$routes->add('home', (new Route('/', ['_controller' => 'Shorty\Controller\HomeController']))->setMethods('GET'));
+$routes->add('redirect_tag', (new Route('/{tag}', ['_controller' => 'Shorty\Controller\TagRedirectController']))->setMethods('GET'));
 
 $context = new RequestContext();
 $context->fromRequest($request);
 
 $matcher = new UrlMatcher($routes, $context);
 
+$resolver = new ControllerResolver();
+
 try {
-	$attributes = $matcher->match($request->getPathInfo());
+	$request->attributes->add($matcher->match($request->getPathInfo()));
+
+	$controller = $resolver->getController($request);
+	die($controller);
+
 }
 catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $e) {
 	die('Unknown Request');
@@ -40,8 +47,8 @@ catch (\Symfony\Component\Routing\Exception\MethodNotAllowedException $e) {
 	die('Unknown Request');
 }
 
-$controller = $attributes['controller'];
-$controller = $container->make($controller);
-
-$response = $controller($request, $session);
-$response->prepare($request)->send();
+//$controller = $attributes['controller'];
+//$controller = $container->make($controller);
+//
+//$response = $controller($request, $session);
+//$response->prepare($request)->send();
